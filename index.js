b723295121f126e381086caeabd33672630d7e59
Justin Greene <greenejustint@gmail.com>
2020-03-07 21:42:33 -0800

    add timeout and guild only messages

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
 
 client.once("ready", () => {
   console.log("Starting DiscordSimulator...");
@@ -26,6 +27,37 @@ client.on("message", message => {
   if (!client.commands.has(commandName)) return;
 
   const command = client.commands.get(commandName);
+
+  if (command.guildOnly && message.channel.type !== "text") {
+    return message.reply("Please use this command in a server.");
+  }
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      if (debug) {
+        return message.reply(
+          `Debug: command on timeout for ${timeLeft} seconds`
+        );
+      } else {
+        return;
+      }
+    }
+  } else {
+    timestamps.set(message.author.id, now);
+    setTimeout(() => {
+      timestamps.delete(message.author.id, cooldownAmount);
+    }, cooldownAmount);
+  }
 
   try {
     command.execute(message, args);
